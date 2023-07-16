3f61db905432b8025bbb140fe0abf296506e74bc
aditi519 <aditipawar510@gmail.com>
2023-07-16 14:12:44 +0530

    Add project files.

---
+++ W/Controllers/Employee.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class Employee : Controller
+    {
+        public string Email;
+        public int Id;
+        public string Name;
+
+        public IActionResult Index()
+        {
+            // Replace this with fetching actual employee data from a database or other data source.
+            var employees = new List<Employee>
+        {
+            new Employee { Id = 1, Name = "ABC", Email = "abc@123.com" },
+            new Employee { Id = 2, Name = "XYZ", Email = "xyz@789.com" }
+        };
+
+            return View(employees);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ W/Controllers/Student.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class Student : Controller
+    {
+        private int Id;
+        private string Name;
+        private string Email;
+
+        public IActionResult Index()
+        {
+            
+            var students = new List<Student>
+        {
+            new Student { Id = 1, Name = "A", Email = "A@123.com" },
+            new Student { Id = 2, Name = "B", Email = "B@123.com" }
+        };
+
+            return View(students);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+
+    }
+}
